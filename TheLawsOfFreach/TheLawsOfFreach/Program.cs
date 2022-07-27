264b7918ced80e528e1c96ce79c429d848314fbc
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-07-27 11:26:45 +0100

    Laws of Freach work - Using foreach to loop through an array, rather than a for loop.

---
+++ TheLawsOfFreach/TheLawsOfFreach/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int total = 0;
+float average = 0;
+
+Array.Sort(array);
+foreach (int index in array)
+{
+    Console.Write(index + ", ");
+
+    total += index;
+    average = (float)total / array.Length;
+};
+Console.WriteLine(average);
+Console.ReadLine();
