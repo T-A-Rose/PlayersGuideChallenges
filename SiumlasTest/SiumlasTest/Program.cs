5cb430c051e83fd0645ea6e924b31732ad03cbf6
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-08-05 16:42:24 +0100

    Work on Simulas Test, not quite grasping this challenge yet!

---
+++ SiumlasTest/SiumlasTest/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Chest chestOpen = Chest.Open;
+Chest chestClosed = Chest.Closed;
+Chest chestLocked = Chest.Locked;
+while (true)
+{
+    Console.WriteLine($"Please enter a command to maniuplate the current state of the chest!");
+    if ()
+    {
+
+    }
+};
+
+enum Chest { Open, Closed, Locked };
