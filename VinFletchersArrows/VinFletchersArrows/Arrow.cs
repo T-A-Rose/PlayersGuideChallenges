2b8ec8981a0e2318e028bbb3502550faff3b95e7
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-08-09 16:59:49 +0100

    Vin Fletchers Arrows attempt, still working on it.

---
+++ VinFletchersArrows/VinFletchersArrows/Arrow.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinFletchersArrows
+{
+    class Arrow
+    {
+        int arrowHeadCost = 0;
+        int arrowFletchingCost = 0;
+        int _arrowShaft;
+        ArrowHead _arrowHead;
+        Fletching _fletching;
+        public Arrow()
+        {
+            _arrowShaft = 60;
+            _arrowHead = ArrowHead.steel;
+            _fletching = Fletching.plastic;
+        }
+
+        public float GetCost()
+        {
+            ChooseArrowHead();
+            ChooseArrowFletching();
+            GetArrowShaft();
+            return (float)((arrowHeadCost + arrowFletchingCost) + (0.05 * _arrowShaft));
+        }
+
+        ArrowHead ChooseArrowHead()
+        {
+            Console.WriteLine($"Please select the arrow type you want to use, the options are as follows: {ArrowHead.steel}, {ArrowHead.wood} & {ArrowHead.obsidian}.");
+            string arrowHeadChoice = Console.ReadLine().ToLower();
+            ArrowHead currentSelection = ArrowHead.steel;
+
+            switch (arrowHeadChoice)
+            {
+                case "steel":
+                    arrowHeadCost = 10;
+                    return ArrowHead.steel;
+                case "wood":
+                    arrowHeadCost = 3;
+                    currentSelection = ArrowHead.wood;
+                    break;
+                case "obsidian":
+                    arrowHeadCost = 5;
+                    currentSelection = ArrowHead.obsidian;
+                    break;
+                default:
+                    break;
+            }
+            return currentSelection;
+        }
+
+        
+
+        int GetArrowShaft()
+        {
+            Console.WriteLine($"Now we have your selected arrow type of {_arrowHead} with a {_fletching}, please enter the length of the shaft you want, between 60 - 100 cm's");
+            _arrowShaft = Convert.ToInt32(Console.ReadLine());
+            return _arrowShaft;
+        }
+    }
+
+
+
+}
---
+++ VinFletchersArrows/VinFletchersArrows/ArrowFletching.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinFletchersArrows
+{
+    internal class ArrowFletching
+    {
+
+        public int arrowFletchingCost { get; set; }
+
+        Fletching ChooseArrowFletching()
+        {
+            Console.WriteLine($"Please select the arrow fletching you want to use, the options are as follows: {Fletching.plastic}, {Fletching.turkeyFeathers} & {Fletching.gooseFeathers}.");
+            string arrowHeadChoice = Console.ReadLine().ToLower();
+            Fletching currentSelection = Fletching.plastic;
+
+            switch (arrowHeadChoice)
+            {
+                case "plastic":
+                    arrowFletchingCost = 10;
+                    return Fletching.plastic;   
+                case "turkey feathers":
+                    arrowFletchingCost = 5;
+                    return Fletching.turkeyFeathers;
+                case "goose feathers":
+                    arrowFletchingCost = 3;
+                    return Fletching.gooseFeathers;                   
+                default:
+                    break;
+            }
+            
+        }
+    }
+}
+
+enum Fletching { plastic, turkeyFeathers, gooseFeathers }
