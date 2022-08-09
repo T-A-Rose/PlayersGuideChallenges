384dadbf546c107bd47b03c495e2db65cb22e7ca
TomAndrew93 <RekAkATom@gmail.com>
2022-08-10 00:02:19 +0100

    Restarted the arrow challenge.

--- VinFletchersArrows/VinFletchersArrows/Arrow.cs
+++ VinFletchersArrows/VinFletchersArrows/Arrow.cs
@@ -8,61 +8,23 @@ namespace VinFletchersArrows
 {
     class Arrow
     {
-        int arrowHeadCost = 0;
-        int arrowFletchingCost = 0;
-        int _arrowShaft;
-        ArrowHead _arrowHead;
-        Fletching _fletching;
-        public Arrow()
+        public ArrowHeadType _arrowHeadType;
+        public ArrowFletchingType _fletchingType;
+        public float _length;
+
+        public Arrow(ArrowHeadType arrowheadType, ArrowFletchingType fletchingType, float length)
         {
-            _arrowShaft = 60;
-            _arrowHead = ArrowHead.steel;
-            _fletching = Fletching.plastic;
+            _arrowHeadType = arrowheadType;
+            _fletchingType = fletchingType;
+            _length = length;
         }
 
-        public float GetCost()
-        {
-            ChooseArrowHead();
-            ChooseArrowFletching();
-            GetArrowShaft();
-            return (float)((arrowHeadCost + arrowFletchingCost) + (0.05 * _arrowShaft));
-        }
-
-        ArrowHead ChooseArrowHead()
-        {
-            Console.WriteLine($"Please select the arrow type you want to use, the options are as follows: {ArrowHead.steel}, {ArrowHead.wood} & {ArrowHead.obsidian}.");
-            string arrowHeadChoice = Console.ReadLine().ToLower();
-            ArrowHead currentSelection = ArrowHead.steel;
-
-            switch (arrowHeadChoice)
-            {
-                case "steel":
-                    arrowHeadCost = 10;
-                    return ArrowHead.steel;
-                case "wood":
-                    arrowHeadCost = 3;
-                    currentSelection = ArrowHead.wood;
-                    break;
-                case "obsidian":
-                    arrowHeadCost = 5;
-                    currentSelection = ArrowHead.obsidian;
-                    break;
-                default:
-                    break;
-            }
-            return currentSelection;
-        }
-
-        
-
-        int GetArrowShaft()
-        {
-            Console.WriteLine($"Now we have your selected arrow type of {_arrowHead} with a {_fletching}, please enter the length of the shaft you want, between 60 - 100 cm's");
-            _arrowShaft = Convert.ToInt32(Console.ReadLine());
-            return _arrowShaft;
-        }
+        //public float GetCost()
+        //{
+        //    return (float)((arrowHeadCost + arrowFletchingCost) + (0.05 * _arrowShaft));
+        //}
     }
 
-
-
+    enum ArrowHeadType { steel, wood, obsidian};
+    enum ArrowFletchingType { plastic, turkeyfeathers, goosefeathers};
 }
