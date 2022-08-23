38fb4a28461fd16b30b7cccd74ac92561fb37ffe
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-08-23 16:43:07 +0100

    Working with 3 static methods, each creating a pre-designed arrow and then presenting the user with a choice to create their own or choose from the pre-designed. Then calculating the cost for the respective arrow.

---
+++ ArrowFactories/ArrowFactories/Arrow.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrowFactories
+{
+    class Arrow
+    {
+        public Arrow(ArrowHeadType arrowheadType, ArrowFletchingType fletchingType, float length)
+        {
+            ArrowHeadType = arrowheadType;
+            ArrowFletchingType = fletchingType;
+            Length = length;
+        }
+
+        public Arrow(){
+        }
+
+        private ArrowHeadType ArrowHeadType { get; }
+        private ArrowFletchingType ArrowFletchingType { get; }
+        private float Length { get; }
+        internal float GetCost => ((GetArrowHeadCost(ArrowHeadType) + GetArrowFletchingCost(ArrowFletchingType)) + (Length* 0.05f));
+        
+        private float GetArrowHeadCost(ArrowHeadType AHT)
+        {
+            switch (AHT)
+            {
+                case ArrowHeadType.steel:
+                    return 10;
+                case ArrowHeadType.wood:
+                    return 3;
+                case ArrowHeadType.obsidian:
+                    return 5;
+                default:
+                    return 0;
+            }
+        }
+
+        private float GetArrowFletchingCost(ArrowFletchingType AFT)
+        {
+            switch (AFT)
+            {
+                case ArrowFletchingType.plastic:
+                    return 10;
+                case ArrowFletchingType.turkeyfeathers:
+                    return 5;
+                case ArrowFletchingType.goosefeathers:
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+
+        public static Arrow CreateEliteArrow() => new Arrow(ArrowHeadType.steel, ArrowFletchingType.plastic, 95);
+        public static Arrow CreateBeginnerArrow() => new Arrow(ArrowHeadType.wood, ArrowFletchingType.goosefeathers, 75);
+        public static Arrow CreateMarksmenArrow() => new Arrow(ArrowHeadType.steel, ArrowFletchingType.goosefeathers, 65);
+
+    }
+
+    public enum ArrowHeadType { steel, wood, obsidian };
+    public enum ArrowFletchingType { plastic, turkeyfeathers, goosefeathers };
+}
---
+++ ArrowFactories/ArrowFactories/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArrowFactories;
+
+Console.WriteLine("Hello adventurer! Welcome to Vin's Arrows!");
+Console.WriteLine("Here we make bespoke custom arrows of your own design, or we can make you some pre-designed arrows, which would you like?");
+
+ArrowHeadType SelectArrowhead()
+{
+    Console.WriteLine($"The options for arrow head are as follows {ArrowHeadType.steel}, {ArrowHeadType.wood} & finally {ArrowHeadType.obsidian}!");
+
+    string chosenArrowhead = Console.ReadLine().ToLower();
+    return chosenArrowhead switch
+    {
+        "steel" => ArrowHeadType.steel,
+        "wood" => ArrowHeadType.wood,
+        "obsidian" => ArrowHeadType.obsidian,
+        _ => ArrowHeadType.steel
+    };
+}
+
+ArrowFletchingType SelectArrowFletching()
+{
+    Console.WriteLine("What fletching would you like to go with that?");
+    Console.WriteLine($"The options are as follows: {ArrowFletchingType.plastic}, {ArrowFletchingType.turkeyfeathers} & finally {ArrowFletchingType.goosefeathers}!");
+
+    string chosenArrowfletching = Console.ReadLine().ToLower();
+    switch (chosenArrowfletching)
+    {
+        case "plastic": return ArrowFletchingType.plastic;
+        case "turkey feathers": return ArrowFletchingType.turkeyfeathers;
+        case "goose feathers": return ArrowFletchingType.goosefeathers;
+        default: return ArrowFletchingType.plastic;
+    }
+}
+
+float SetArrowLength()
+{
+    Console.WriteLine("Now, we just need the length of the arrow you want!");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void DisplayCost(Arrow arrow)
+{
+    Console.WriteLine($"The cost for that arrow will be {arrow.GetCost} gold pieces!");
+}
+
+string bespokeOrCustom = Console.ReadLine().ToLower();
+
+if (bespokeOrCustom == "custom")
+{
+    DisplayCost(new Arrow(SelectArrowhead(), SelectArrowFletching(), SetArrowLength()));
+} else
+{
+    Console.WriteLine("Your pre-designed options are as follows: Elite Arrow, Beginner Arrow or finally Marksmen Arrow");
+    string chosenPreDesigned = Console.ReadLine().ToLower();
+    Arrow arrow = chosenPreDesigned switch
+    {
+        "elite arrow" => Arrow.CreateEliteArrow(),
+        "beginner arrow" => Arrow.CreateEliteArrow(),
+        "marksmen arrow" => Arrow.CreateMarksmenArrow(),
+        _ => Arrow.CreateEliteArrow()
+    };
+    DisplayCost(arrow);
+}
+
+Console.ReadLine();
+
