229ee2398f5fa1c8423db1cb61cf29d945b21f7a
TomAndrew93 <RekAkATom@gmail.com>
2022-08-23 10:30:34 +0100

    ?

--- ThePropertiesOfArrows/ThePropertiesOfArrows/Arrow.cs
+++ ThePropertiesOfArrows/ThePropertiesOfArrows/Arrow.cs
@@ -8,10 +8,6 @@ namespace VinFletchersArrows
 {
     class Arrow
     {
-        //private ArrowHeadType _arrowHeadType;
-        //private ArrowFletchingType _fletchingType;
-        //private float Length;
-
         public Arrow(ArrowHeadType arrowheadType, ArrowFletchingType fletchingType, float length)
         {
             ArrowHeadType = arrowheadType;
@@ -22,26 +18,21 @@ namespace VinFletchersArrows
         public Arrow(){
         }
 
-        //private ArrowHeadType GetArrowHeadType() => _arrowHeadType;
-        //private ArrowFletchingType GetArrowFletchingType() => _fletchingType;
-        //private float GetLength() => _length;
-
         private ArrowHeadType ArrowHeadType { get; }
         private ArrowFletchingType ArrowFletchingType { get; }
         private float Length { get; }
-
         internal float GetCost => ((GetArrowHeadCost(ArrowHeadType) + GetArrowFletchingCost(ArrowFletchingType)) + (Length* 0.05f));
+        
         private float GetArrowHeadCost(ArrowHeadType AHT)
         {
-            float arrowHTCost = 0;
             switch (AHT)
             {
                 case ArrowHeadType.steel:
-                    return arrowHTCost = 10;
+                    return 10;
                 case ArrowHeadType.wood:
-                    return arrowHTCost = 3;
+                    return 3;
                 case ArrowHeadType.obsidian:
-                    return arrowHTCost = 5;
+                    return 5;
                 default:
                     return 0;
             }
@@ -49,22 +40,20 @@ namespace VinFletchersArrows
 
         private float GetArrowFletchingCost(ArrowFletchingType AFT)
         {
-            float arrowFTCost = 0;
             switch (AFT)
             {
                 case ArrowFletchingType.plastic:
-                    return arrowFTCost = 10;
+                    return 10;
                 case ArrowFletchingType.turkeyfeathers:
-                    return arrowFTCost = 5;
+                    return 5;
                 case ArrowFletchingType.goosefeathers:
-                    return arrowFTCost = 3;
+                    return 3;
                 default:
                     return 0;
             }
         }
-
-
     }
+
     public enum ArrowHeadType { steel, wood, obsidian };
     public enum ArrowFletchingType { plastic, turkeyfeathers, goosefeathers };
 }
