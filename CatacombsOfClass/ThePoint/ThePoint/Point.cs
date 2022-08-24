5f7e5dc8d71f05a0d6dc8c124fbdb9098f5b7ff2
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-08-24 14:51:31 +0100

    Completing "The Point" boss battle.

---
+++ CatacombsOfClass/ThePoint/ThePoint/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePoint
+{
+    internal class Point
+    {
+        public int _x { get; set; }
+        public int _y { get; set; }
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public Point()
+        {
+            _x = 0;
+            _y = 0;
+        }    
+    }
+}
---
+++ CatacombsOfClass/ThePoint/ThePoint/Program.cs
@@ -0,0 +1,20 @@
+﻿using ThePoint;
+
+public class temp
+{
+    static public void Main(String[] args)
+    {
+        Point point1 = new Point(2, 3);
+        Console.WriteLine($"({point1._x}, {point1._y})");
+
+        Point point2 = new Point(-4, 0);
+        Console.WriteLine($"({point2._x}, {point2._y})");
+
+        Point point3 = new Point();
+        Console.WriteLine($"({point3._x}, {point3._y})");
+
+        Console.ReadLine();
+    }
+
+}
+
