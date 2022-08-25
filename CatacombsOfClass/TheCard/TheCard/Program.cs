f2fb73cb7fee03611c26701ade67f0f499fe5625
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-08-25 16:41:41 +0100

    Unsuccessful work on "The Card" challenge, really struggling with that one!

---
+++ CatacombsOfClass/TheCard/TheCard/Program.cs
@@ -0,0 +1,31 @@
+﻿
+DisplayEachCard();
+
+int DisplayEachCard()
+{
+    foreach (int i in CardColour.GetValues(typeof(CardColour))){
+        Console.WriteLine(i);
+    }
+    return 1;
+}
+
+public class Card
+{
+    CardColour CC;
+    CardRank CR;
+
+    public Card(CardColour _CC, CardRank _CR)
+    {
+        CC = _CC;
+        CR = _CR;
+    }
+
+    public CardColour DetermineCard()
+    {
+        return new CardColour();
+    }
+
+}
+
+public enum CardColour { red, green, blue, yellow};
+public enum CardRank { one = 1, two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8, nine = 9, ten = 10, dollar = 11, percentage = 12, Caret = 13, ampersand = 14};
