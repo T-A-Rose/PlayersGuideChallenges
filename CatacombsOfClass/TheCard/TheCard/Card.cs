0fc64eb1c4154e177af6a66aaa2adb5b3786585a
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-09-15 12:07:37 +0100

    Working "The Card" challenge.

---
+++ CatacombsOfClass/TheCard/TheCard/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCard
+{
+    internal class Card
+    {
+        readonly string CC;
+        readonly string CR;
+
+        public Card(string _CC, string _CR)
+        {
+            CC = _CC;
+            CR = _CR;
+        }
+
+        public void DetermineCard()
+        {
+            Console.WriteLine($"The {CC} {CR}");
+        }
+
+    }
+}
