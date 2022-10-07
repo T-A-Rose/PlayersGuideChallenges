57ffd7c4ac832e24a85dcca4452210e5ad98295e
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-10-07 16:53:19 +0100

    Incomplete "Room Coordinate" challenge

---
+++ RoomCoordinates/RoomCoordinates/Coordinate.cs
@@ -0,0 +1,21 @@
+﻿public struct Coordinate
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Coordinate(int _x, int _y)
+    {
+        X = _x; Y = _y; 
+    }
+
+    public static bool adjacent(Coordinate a, Coordinate b)
+    {
+        int xChange = a.X - b.X;
+        int yChange = a.Y - b.Y;
+
+        if ((xChange <= a.X - 1) && (yChange == 0)) return true;
+        if ((yChange <= a.Y - 1) && (xChange == 0)) return true;
+        return false;
+    }
+
+}
---
+++ RoomCoordinates/RoomCoordinates/MainProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomCoordinates
+{
+    internal class MainProgram
+    {
+
+        static public void Main(String[] args)
+        {
+            Random random = new Random();
+
+            Coordinate a = new Coordinate(1, 2);
+            Coordinate b = new Coordinate(2, 3);
+            Coordinate c = new Coordinate(3, 4);
+
+            Console.WriteLine(Coordinate.adjacent(a, b));
+            Console.WriteLine(Coordinate.adjacent(b, c));
+            Console.WriteLine(Coordinate.adjacent(a, c));
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
