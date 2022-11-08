75d54138d91bf54f0b01d6b26259ff254bf8befe
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-11-08 16:42:50 +0000

    Replicator Of Dto Completed, (horrendously mind)

---
+++ TheReplicatorOfDto (6th Ed)/TheReplicatorOfDto (6th Ed)/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TheReplicatorOfDto
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 5 numbers");
+            int[] firstArray = new int[5];
+            
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                firstArray[i] = num;
+            }
+
+            int[] copiedArray = new int[5];
+            Array.Copy(firstArray, copiedArray, 5);
+
+            DisplayArray(firstArray);
+            DisplayArray(copiedArray);
+            Console.ReadLine();
+        }
+
+        static void DisplayArray(int[] arr)
+        {            
+            foreach (int i in arr)
+            {
+                Console.Write($"{i}");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
