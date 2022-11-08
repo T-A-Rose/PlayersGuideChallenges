6bc7646a47cc434473e840333b2520302930146e
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-11-08 10:47:46 +0000

    Doing the challenge on the beta test 6th edition of the book.

---
+++ LawsOfFreach(6th Ed)/LawsOfFreach(6th Ed)/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currLowestAmt = int.MaxValue;
+int totalSum = 0;
+foreach (int i in array)
+{
+    totalSum += i;
+    if (i < currLowestAmt)
+    {
+        currLowestAmt = i;
+    }
+}
+
+float average = (float)totalSum / array.Length;
+
+Console.WriteLine($"Lowest amount of the array: {currLowestAmt}\n");
+
+Console.WriteLine($"Average amount of the array: {average}");
+Console.ReadLine();
