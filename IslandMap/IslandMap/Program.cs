04d284988387db324f32efa82a9f5778fdf905de
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-11-08 15:18:52 +0000

    Complete multi-dimensional array challenge.

---
+++ IslandMap/IslandMap/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+
+int[,] mapArray = new int[6, 7]
+{
+    {0, 0, 0, 0, 0, 0, 0}, 
+    {0, 1, 0, 1, 1, 0, 0}, 
+    {0, 1, 1, 1, 1, 1, 0}, 
+    {0, 0, 1, 1, 1, 0, 0}, 
+    {0, 1, 1, 0, 1, 0, 0}, 
+    {0, 0, 0, 0, 0, 0, 0}
+};
+
+for (int i = 0; i < mapArray.GetLength(0); i++)
+{
+    for (int j = 0; j < mapArray.GetLength(1); j++)
+    {
+        if (mapArray[i, j] == 0)
+        {
+            Console.Write($" ");
+        } else
+        {
+            Console.Write($"**");
+        }
+    }
+    Console.WriteLine();
+}
+
+Console.ReadLine();
