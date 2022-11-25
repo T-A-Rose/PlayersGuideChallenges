4d694e25f606696e85d8aeccc049f28cf9ecea01
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-11-25 13:03:07 +0000

    Simulas Kitchen Part 1 completion.

---
+++ SimulasKitchenP1(6th Ed)/SimulasKitchenP1(6th Ed)/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+(int, string, string) food;
+food = (7, "-", "Vegetable Soup");
+DisplayFood(food);
+
+food = (3, "-", "Cheese Pizza");
+DisplayFood(food);
+
+void DisplayFood((int, string, string) food)
+{
+    Console.WriteLine($"{food.Item1}{food.Item2}{food.Item3}");
+}
