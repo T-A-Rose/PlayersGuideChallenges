db24bc64550b6b8e1b109025ebb99ed63ed731be
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-11-25 13:12:49 +0000

    Simulas Test Part 2 6th Edition Complete.

--- SimulasKitchenP1(6th Ed)/SimulasKitchenP1(6th Ed)/Program.cs
+++ SimulasKitchenP1(6th Ed)/SimulasKitchenP1(6th Ed)/Program.cs
@@ -1,14 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-(int, string, string) food;
-food = (7, "-", "Vegetable Soup");
+(int, Enum, Enum) food;
+food = (7, ingredient.Vegetable, recipeTypes.Soup);
 DisplayFood(food);
 
-food = (3, "-", "Cheese Pizza");
+food = (3, ingredient.Cheese, recipeTypes.Pizza);
 DisplayFood(food);
 
-void DisplayFood((int, string, string) food)
+food = (3, ingredient.Meat, recipeTypes.Bread);
+DisplayFood(food);
+
+void DisplayFood((int, Enum, Enum) food)
 {
-    Console.WriteLine($"{food.Item1}{food.Item2}{food.Item3}");
+    Console.WriteLine($"{food.Item1}-{food.Item2} {food.Item3}");
 }
+
+enum recipeTypes { Soup, Bread, Pizza};
+enum ingredient { Cheese, Meat, Vegetable}
