edb6afb323989fade5e392ad49b8d566111a0455
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-11-25 12:44:34 +0000

    Passing Simulas Test 6th edition.

---
+++ SimulasTest(6th Ed)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello Adventurer, you come across a chest, this test can be Open, Closed, Locked or Unlocked.");
+ChestState chestState = ChestState.Locked;
+
+while (true)
+{
+    Console.WriteLine("Please enter what you want to do with the box!");
+
+    switch (Console.ReadLine().ToLower())
+    {
+        case "unlock":
+            if (chestState == ChestState.Locked) chestState = ChestState.Unlocked;
+            break;
+        case "open":
+            if (chestState == ChestState.Unlocked) chestState = ChestState.Open;
+            break;
+        case "close":
+            if (chestState == ChestState.Open) chestState = ChestState.Closed;
+            break;
+        case "lock":
+            if (chestState == ChestState.Closed || chestState == ChestState.Unlocked) chestState = ChestState.Locked;
+            break;
+        default:
+            break;
+    }
+    Console.WriteLine($"The state of the chest is {chestState}");
+}
+
+enum ChestState { Open, Closed, Locked, Unlocked};
