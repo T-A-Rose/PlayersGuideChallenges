3962181d55fe05e81373567d22d35ea591d40311
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-11-25 13:18:42 +0000

    Simulas Kitchen Part 3 6th Edition complete.

--- SimulasKitchenP1(6th Ed)/SimulasKitchenP1(6th Ed)/Program.cs
+++ SimulasKitchenP1(6th Ed)/SimulasKitchenP1(6th Ed)/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-(int, Enum, Enum) food;
+(int Number, Enum Ingredient, Enum Base) food;
 food = (7, ingredient.Vegetable, recipeTypes.Soup);
 DisplayFood(food);
 
@@ -11,9 +11,9 @@ DisplayFood(food);
 food = (3, ingredient.Meat, recipeTypes.Bread);
 DisplayFood(food);
 
-void DisplayFood((int, Enum, Enum) food)
+void DisplayFood((int Number, Enum Ingredient, Enum Base) food)
 {
-    Console.WriteLine($"{food.Item1}-{food.Item2} {food.Item3}");
+    Console.WriteLine($"{food.Number}-{food.Ingredient} {food.Base}");
 }
 
 enum recipeTypes { Soup, Bread, Pizza};
