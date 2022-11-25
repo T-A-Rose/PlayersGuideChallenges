cf64e52821c2fd8177da01e3d39d81e6e3eeef2e
TomAndrew93 <T.A.Rose1993@gmail.com>
2022-11-25 15:58:15 +0000

    Vins Fletchers challenge 1 & 2 Completion 6th Ed

---
+++ VinsFletchersArrowsPart1 (6th Ed)/VinsFletchersArrowsPart1 (6th Ed)/Arrow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinsFletchersArrowsPart1__6th_Ed_
+{
+    internal class Arrow
+    {
+        public int Length;
+        public ArrowHead arrowHead;
+        public Fletching fletching;
+
+        public Arrow(int length, ArrowHead arrowHead, Fletching fletching)
+        {
+            Length = length;
+            this.arrowHead = arrowHead;
+            this.fletching = fletching;
+        }
+
+        public float GetCost()
+        {
+            int arrowHeadCost = 0;
+            switch (arrowHead)
+            {
+                case ArrowHead.Steel:
+                        arrowHeadCost = 10;
+                    break;
+                case ArrowHead.Wood:
+                    arrowHeadCost = 3;
+                    break;
+                case ArrowHead.Obsidian:
+                    arrowHeadCost = 5;
+                    break;
+            }
+            return ((float)(arrowHeadCost + (0.05 * Length)));
+        }
+    }
+
+    enum ArrowHead { Steel, Wood, Obsidian};
+    enum Fletching { Plastic, TurkeyFeathers, GooseFeathers};
+}
---
+++ VinsFletchersArrowsPart1 (6th Ed)/VinsFletchersArrowsPart1 (6th Ed)/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VinsFletchersArrowsPart1__6th_Ed_;
+
+Arrow arrow = new(75, ArrowHead.Obsidian, Fletching.TurkeyFeathers);
+DisplayArrow(arrow);
+Console.ReadLine();
+
+void DisplayArrow(Arrow arrow)
+{
+    Console.WriteLine($"Length: {arrow.Length}cm");
+    Console.WriteLine($"Fletching: {arrow.fletching}");
+    Console.WriteLine($"Arrowhead: {arrow.arrowHead}");
+    Console.WriteLine($"Cost: {arrow.GetCost()}gp");
+}
+
+
