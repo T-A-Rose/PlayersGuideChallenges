0c7449453691b67b4b541fabc8e0800ba8ad055d
TomAndrew93 <T.A.Rose1993@gmail.com>
2023-03-14 15:47:27 +0000

    Completing the War Preparations challenge with minor differences from the original request.

---
+++ WarPreperations/WarPreperations/Program.cs
@@ -0,0 +1,23 @@
+﻿using WarPreperations;
+
+List<Sword> listOfSwords = new();
+
+var ironSword = new Sword(Materials.iron, Gemstone.none, 100, 25);
+var bronzeSword = new Sword(Materials.bronze, Gemstone.emerald, 75, 10);
+var steelSword = new Sword(Materials.steel, Gemstone.diamond, 125, 15);
+
+creatingListOfSwords(ironSword);
+creatingListOfSwords(bronzeSword);
+creatingListOfSwords(steelSword);
+
+List<Sword> creatingListOfSwords(Sword sword)
+{
+    listOfSwords.Add(sword);
+    return listOfSwords;
+}
+
+foreach (var sword in listOfSwords)
+{
+    Console.WriteLine(sword.ToString());
+}
+Console.ReadLine();
---
+++ WarPreperations/WarPreperations/Sword.cs
@@ -0,0 +1,22 @@
+﻿namespace WarPreperations;
+
+public record Sword(Materials materials, Gemstone gemstone, int length, int crossguardWidth)
+{
+    public override string ToString()
+    {
+        if (gemstone == Gemstone.none)
+        {
+            return $"A {length} inch blade with a {crossguardWidth} cm crossguard" +
+                   $", made primarily out of {materials} with no gemstone";    
+        }
+        else
+        {
+            return $"A {length} inch blade with a {crossguardWidth} cm crossguard" +
+                   $", made primarily out of {materials} with an {gemstone} gemstone";
+        }
+    }    
+}
+
+public enum Materials { wood, bronze, iron, steel, binarium};
+
+public enum Gemstone { emerald, amber, sapphire, diamond, bitstone, none}
